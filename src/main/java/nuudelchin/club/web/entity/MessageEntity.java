0b4d3debd23444c88e24d26a6c13a50b0edffc9f
lkhaajav <lkhaching@gmail.com>
2025-01-16 09:59:56 +0900

    websocket, chat, message

--- src/main/java/nuudelchin/club/web/entity/MessageEntity.java
+++ src/main/java/nuudelchin/club/web/entity/MessageEntity.java
@@ -3,7 +3,6 @@ package nuudelchin.club.web.entity;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
-import nuudelchin.club.web.dto.MessageDTO;
 
 @Entity
 @Setter
@@ -14,13 +13,4 @@ public class MessageEntity {
     private String senderId;
     private String recipientId;
     private String content;
-    
-    public MessageDTO convertToMessageDTO() {
-    	
-    	MessageDTO dto = new MessageDTO();
-    	dto.setSenderId(senderId);
-    	dto.setContent(content);
-		
-		return dto;
-    }
 }
--- src/main/java/nuudelchin/club/web/service/MessageService.java
+++ src/main/java/nuudelchin/club/web/service/MessageService.java
@@ -1,13 +1,10 @@
 package nuudelchin.club.web.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
-import nuudelchin.club.web.dto.MessageDTO;
-import nuudelchin.club.web.dto.UserDTO;
 import nuudelchin.club.web.entity.ChatEntity;
 import nuudelchin.club.web.entity.MessageEntity;
 import nuudelchin.club.web.repository.MessageRepository;
@@ -18,18 +15,15 @@ public class MessageService {
 	private final SimpMessagingTemplate messagingTemplate;
 	private final MessageRepository messageRepository;
 	private final ChatService chatService;
-	private final UserService userService;
 
     public MessageService(
     		SimpMessagingTemplate messagingTemplate, 
     		MessageRepository messageRepository, 
-    		ChatService chatService,
-    		UserService userService) {
+    		ChatService chatService) {
 
     	this.messagingTemplate = messagingTemplate;
         this.messageRepository = messageRepository;
         this.chatService = chatService;
-        this.userService = userService;
     }
     
     public void send(MessageEntity entity) {
@@ -42,15 +36,9 @@ public class MessageService {
     	int result = messageRepository.save(entity);
     	if(result == 1) {
     		
-    		MessageDTO dto = entity.convertToMessageDTO();    		
-    		UserDTO sender = userService.findByUsername(dto.getSenderId());
-    		
-    		dto.setFullname(sender.getFullname());
-    		dto.setPicture(sender.getPicture());
-    		
     		messagingTemplate.convertAndSendToUser(
         		entity.getRecipientId(), "/queue/messages",
-        		dto
+        		entity
 	        );
     	} else {
     		
@@ -61,31 +49,12 @@ public class MessageService {
     	}
     }
     
-    public List<MessageDTO> get(ChatEntity entity) {
+    public List<MessageEntity> get(ChatEntity entity) {
     	
     	String senderId = entity.getSenderId();
     	String recipientId = entity.getRecipientId();
     	String chatId = chatService.getChatId(senderId, recipientId);  
     	
-    	List<MessageEntity> entityList = messageRepository.findByChatId(chatId);
-    	
-    	List<MessageDTO> dtoList = new ArrayList<MessageDTO>(); 
-    	
-    	int i, isize = entityList.size();
-    	for(i = 0; i < isize; i++) {
-    		
-//    		MessageDTO dto = entityList.get(i).convertToMessageDTO();    		
-//    		UserDTO sender = userService.findByUsername(dto.getSenderId());
-//    		
-//    		dto.setFullname(sender.getFullname());
-//    		dto.setPicture(sender.getPicture());
-    		
-    		MessageDTO dto = new MessageDTO();
-    		dto.setContent(entityList.get(i).getContent());
-    		
-    		dtoList.add(dto);
-    	}
-    	
-    	return dtoList;
+    	return messageRepository.findByChatId(chatId);
 	}
 }
